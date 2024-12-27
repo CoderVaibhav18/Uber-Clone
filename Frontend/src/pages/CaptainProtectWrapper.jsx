c296d7bb22b3597a381928ea3ec0b1824e180695
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2024-12-28 01:04:25 +0530

    Captain's home protection done

--- Frontend/src/pages/CaptainProtectWrapper.jsx
+++ Frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,26 +1,46 @@
 import PropTypes from "prop-types";
-import { useEffect } from "react";
-
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CaptainData } from "../context/CaptainContext";
+import axios from "axios";
 
-const CaptainProtectWrapper = ({children}) => {
+const CaptainProtectWrapper = ({ children }) => {
   const token = localStorage.getItem("token");
   const Navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { captain, setCaptain } = useContext(CaptainData);
 
   useEffect(() => {
     if (!token) {
       Navigate("/captain-login");
     }
-  }, [token, Navigate]);
-  
-  return (
-    <>
-      {children}
-    </>
-  )
-}
+
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/captain/profile`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setCaptain(response.data.captain);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        localStorage.removeItem("token");
+        Navigate("/captain-login");
+      });
+  }, [token, Navigate, setCaptain]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <>{children}</>;
+};
 CaptainProtectWrapper.propTypes = {
   children: PropTypes.node.isRequired, // Ensures 'children' is a valid React node and required
 };
 
-export default CaptainProtectWrapper
+export default CaptainProtectWrapper;
