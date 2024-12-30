cca0dda518bfbe5680f7379e6dcf31126dde7548
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2024-12-30 21:45:11 +0530

    home page approximately done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,32 +4,24 @@ const Home = () => {
   return (
     <div className="h-screen relative">
       <img
-        className="w-16 absolute top-5 left-5"
+        className="w-16 top-5 left-5 absolute"
         src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt="uber logo"
       />
 
-      <div className="h-screen w-screen ">
+      <div className="h-screen w-screen">
         <img
-          className="w-full h-full object-cover"
-          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
-        />
+        className="w-full h-full object-cover" src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" />
       </div>
 
-      <div className="bg-white p-5 absolute bottom-0 ">
+      <div className="bg-white p-5 bottom-0 absolute w-screen">
         <h3 className="text-2xl font-semibold">Find a trip</h3>
         <form>
-          <input
-            className="bg-[#eeee] mt-3 text-lg w-full rounded-lg  px-8 py-2"
-            type="text"
-            placeholder="Add a pick-up location"
-          />
-          <input
-            className="bg-[#eeee] mt-3 text-lg w-full rounded-lg  px-8 py-2"
-            type="text"
-            placeholder="Add your destination"
-          />
+          <input type="text" placeholder="Add a pick-up location" className=" bg-[#eeee] rounded-lg text-lg mt-4 px-12 py-2 w-full"/>
+          <input type="text" placeholder="Add your destination" className=" bg-[#eeee] rounded-lg text-lg mt-4 px-12 py-2 w-full"/>
         </form>
       </div>
+
     </div>
   );
 };
