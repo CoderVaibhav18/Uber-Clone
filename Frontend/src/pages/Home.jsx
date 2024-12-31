7a2df879483b07c7462c51a8176f171feaa4eb7d
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2024-12-31 18:50:06 +0530

    Add user home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,25 +1,24 @@
-import gsap from 'gsap'
-import {useGSAP} from '@gsap/react'
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { useRef, useState } from "react";
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
-  const [panelOpen, setPanelOpen] = useState(false)
-  const panelRef = useRef(null)
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
 
   useGSAP(() => {
-    if(panelOpen){
+    if (panelOpen) {
       gsap.to(panelRef.current, {
-        height: '70%'
-      })
-    }else{
+        height: "70%",
+      });
+    } else {
       gsap.to(panelRef.current, {
-        height: '0'
-      })
+        height: "0",
+      });
     }
-  },[panelOpen])
-
+  }, [panelOpen]);
 
   return (
     <div className="h-screen relative w-screen">
@@ -30,7 +29,7 @@ const Home = () => {
 
       <div className="h-screen w-screen">
         <img
-          className="h-screen object-cover"
+          className="h-screen  w-screen object-cover"
           src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
         />
       </div>
@@ -39,7 +38,7 @@ const Home = () => {
         <div className="h-[30%] bg-white p-6 relative">
           <h4 className="text-2xl font-semibold">Find a trip</h4>
           <form>
-            <div className="line h-16 w-0.5 bg-gray-900 absolute top-[45%] left-10"></div>
+            <div className="line h-16 w-1 rounded-full bg-gray-900 absolute top-[45%] left-10"></div>
             <input
               className="bg-[#eeeeeec7] rounded-lg text-base mt-3 w-full px-12 py-2"
               type="text"
