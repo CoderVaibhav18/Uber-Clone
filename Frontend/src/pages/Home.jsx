4ca9fcf2691563b9d80757fd2a6da19a5dd707a4
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-01 21:01:19 +0530

    Add location search panel

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
+// import React from 'react'
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import "remixicon/fonts/remixicon.css";
 import { useRef, useState } from "react";
+import "remixicon/fonts/remixicon.css";
 import LocationSearchPanel from "../components/LocationSearchPanel";
 
 const Home = () => {
@@ -9,73 +10,74 @@ const Home = () => {
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
   const panelRef = useRef(null);
-  const panelClose = useRef(null);
+  const panelCloseRef = useRef(null)
 
   useGSAP(() => {
     if (panelOpen) {
       gsap.to(panelRef.current, {
         height: "70%",
-        padding: 20,
+        padding: 20
       });
-      gsap.to(panelClose.current, {
+      gsap.to(panelCloseRef.current, {
         opacity: 1,
       });
     } else {
       gsap.to(panelRef.current, {
         height: "0",
-        padding: 0,
+        padding: 0
       });
-      gsap.to(panelClose.current, {
+      gsap.to(panelCloseRef.current, {
         opacity: 0,
       });
     }
-  }, [panelOpen]);
+  }, [panelOpen, panelCloseRef]);
 
   return (
-    <div className="h-screen relative w-screen">
+    <div className="h-screen relative">
       <img
-        className="w-16 absolute top-5 left-5"
+        className="w-20 absolute top-6 left-6"
         src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt="uber logo"
       />
 
       <div className="h-screen w-screen">
         <img
-          className="h-screen  w-screen object-cover"
+          className="h-full w-full  object-cover"
           src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
         />
       </div>
 
-      <div className="flex flex-col justify-end h-screen absolute bottom-0 w-full">
-        <div className="h-[30%] bg-white p-6 relative">
+      <div className="flex flex-col justify-end h-screen w-full absolute bottom-0">
+        <div className="bg-white h-[30%] p-6 relative">
           <h4
-            ref={panelClose}
+          ref={panelCloseRef}
             onClick={() => setPanelOpen(false)}
-            className="absolute opacity-0 right-6 top-7 text-3xl"
+            className="font-bold text-3xl absolute right-6"
           >
-            <i className="ri-arrow-down-wide-line"></i>
+            <i className="ri-arrow-up-wide-line"></i>
           </h4>
           <h4 className="text-2xl font-semibold">Find a trip</h4>
           <form>
-            <div className="line h-14 w-1 rounded-full bg-gray-900 absolute top-[42%] left-10"></div>
+            <div className="line h-16 w-1 bg-gray-900 rounded-full absolute top-[43%] left-10"></div>
             <input
-              className="bg-[#eeeeeec7] rounded-lg text-base mt-3 w-full px-12 py-2"
               type="text"
               onClick={() => setPanelOpen(true)}
               value={pickup}
               onChange={(e) => setPickup(e.target.value)}
-              placeholder="Add a pick-up location"
+              placeholder="Add your pick-up location"
+              className="px-12 py-2 text-lg mt-3 w-full rounded-lg"
             />
             <input
-              className="bg-[#eeeeeec7] rounded-lg text-base mt-3 w-full px-12 py-2"
               type="text"
               onClick={() => setPanelOpen(true)}
+              placeholder="Add your destination"
+              className="px-12 py-2 text-lg mt-3 w-full rounded-lg"
               value={destination}
               onChange={(e) => setDestination(e.target.value)}
-              placeholder="Add your destination"
             />
           </form>
         </div>
-        <div ref={panelRef} className=" bg-white">
+        <div ref={panelRef} className="bg-white">
           <LocationSearchPanel />
         </div>
       </div>
