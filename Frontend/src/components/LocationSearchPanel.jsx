1227a7afb3b1e8c920b845846eefb86a7b624612
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-02 22:09:32 +0530

    Refactor LocationSearchPanel and Home page components with minor styling changes

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -4,7 +4,7 @@ import 'remixicon/fonts/remixicon.css'
 const LocationSearchPanel = () => {
   return (
     <div>
-      <div className="gap-3 flex items-center my-4 justify-start">
+      <div className="gap-3 border-2 border-white active:border-black rounded-xl flex items-center my-2 p-3 justify-start">
         <h2 className="bg-[#eeee] h-8 w-12 flex items-center justify-center rounded-full">
           <i className="ri-map-pin-fill"></i>
         </h2>
@@ -12,7 +12,7 @@ const LocationSearchPanel = () => {
           167, Killa Ward Ballarpur-442701, Dist Chandrapur, Near Visapur Road
         </h4>
       </div>
-      <div className="gap-3 flex items-center my-4 justify-start">
+      <div className="gap-3 border-2 border-white active:border-black rounded-xl flex items-center  my-2 p-3 justify-start">
         <h2 className="bg-[#eeee] h-8 w-12 flex items-center justify-center rounded-full">
           <i className="ri-map-pin-fill"></i>
         </h2>
@@ -20,7 +20,7 @@ const LocationSearchPanel = () => {
           167, Killa Ward Ballarpur-442701, Dist Chandrapur, Near Visapur Road
         </h4>
       </div>
-      <div className="gap-3 flex items-center my-4 justify-start">
+      <div className="gap-3 border-2 border-white active:border-black rounded-xl flex items-center  my-2 p-3 justify-start">
         <h2 className="bg-[#eeee] h-8 w-12 flex items-center justify-center rounded-full">
           <i className="ri-map-pin-fill"></i>
         </h2>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -82,14 +82,14 @@ const Home = () => {
           <LocationSearchPanel />
         </div>
       </div>
-      <div className="fixed w-full bg-white bottom-0 z-10 px-3 py-6">
+      <div className="fixed w-full bg-white translate-y-full bottom-0 z-10 px-3 py-6">
         <h3 className="text-2xl font-semibold mb-5">Choose vehicle</h3>
-        <div className="flex items-center border-2 border-black rounded-lg justify-between p-3 mb-2 w-full">
+        <div className="flex items-center border-2 active:border-black rounded-lg justify-between p-3 mb-2 w-full">
           <img
-            className="h-12"
+            className="h-10"
             src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg"
           />
-          <div className=" w-1/2">
+          <div className="-ml-2 w-1/2">
             <h4 className="font-medium text-base">
               UberGo{" "}
               <span>
@@ -97,27 +97,45 @@ const Home = () => {
               </span>
             </h4>
             <h5 className="font-medium text-sm">2 mins away</h5>
-            <p className="font-medium text-xs">Affortable, compact price</p>
+            <p className="font-medium text-xs">Affortable, compact rides</p>
           </div>
-          <h4 className="font-semibold text-xl">₹193.2</h4>
+          <h4 className="font-semibold text-lg">₹193.2</h4>
         </div>
-        <div className="flex items-center border-2 border-black rounded-lg justify-between p-3 mb-2 w-full">
+        <div className="flex items-center border-2 active:border-black rounded-lg justify-between p-3 mb-2 w-full">
           <img
-            className="h-12"
-            src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg"
+            className="h-10"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
           />
-          <div className=" w-1/2">
+          <div className="-ml-2 w-1/2">
             <h4 className="font-medium text-base">
-              UberGo{" "}
+              Moto{" "}
+              <span>
+                <i className="ri-user-3-fill"></i>1
+              </span>
+            </h4>
+            <h5 className="font-medium text-sm">3 mins away</h5>
+            <p className="font-medium text-xs">Affortable, motorcycle rides</p>
+          </div>
+          <h4 className="font-semibold text-lg">₹65.17</h4>
+        </div>
+        <div className="flex items-center border-2 active:border-black rounded-lg justify-between p-3 mb-2 w-full">
+          <img
+            className="h-10"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+          />
+          <div className="-ml-2 w-1/2">
+            <h4 className="font-medium text-base">
+              UberAuto{" "}
               <span>
                 <i className="ri-user-3-fill"></i>3
               </span>
             </h4>
             <h5 className="font-medium text-sm">2 mins away</h5>
-            <p className="font-medium text-xs">Affortable, compact price</p>
+            <p className="font-medium text-xs">Affortable, Auto rides</p>
           </div>
-          <h4 className="font-semibold text-xl">₹193.2</h4>
+          <h4 className="font-semibold text-lg">₹118.86</h4>
         </div>
+        
       </div>
     </div>
   );
