921c227264810228fa5c3307165dab1c6a6f48a0
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-02 22:38:03 +0530

    use an array of locations and map over it

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,33 +1,27 @@
 // import React from 'react'
-import 'remixicon/fonts/remixicon.css'
+import "remixicon/fonts/remixicon.css";
 
 const LocationSearchPanel = () => {
+  const locations = [
+    "167, Killa Ward Ballarpur-442701, Dist Chandrapur, Near Visapur Road",
+    "168, Sidharth Ward Ballarpur-442701, Dist Chandrapur",
+    "169, Jay bhim chok Ward Ballarpur-442701, Dist Chandrapur",
+  ];
   return (
     <div>
-      <div className="gap-3 border-2 border-white active:border-black rounded-xl flex items-center my-2 p-3 justify-start">
-        <h2 className="bg-[#eeee] h-8 w-12 flex items-center justify-center rounded-full">
-          <i className="ri-map-pin-fill"></i>
-        </h2>
-        <h4 className="font-medium">
-          167, Killa Ward Ballarpur-442701, Dist Chandrapur, Near Visapur Road
-        </h4>
-      </div>
-      <div className="gap-3 border-2 border-white active:border-black rounded-xl flex items-center  my-2 p-3 justify-start">
-        <h2 className="bg-[#eeee] h-8 w-12 flex items-center justify-center rounded-full">
-          <i className="ri-map-pin-fill"></i>
-        </h2>
-        <h4 className="font-medium">
-          167, Killa Ward Ballarpur-442701, Dist Chandrapur, Near Visapur Road
-        </h4>
-      </div>
-      <div className="gap-3 border-2 border-white active:border-black rounded-xl flex items-center  my-2 p-3 justify-start">
-        <h2 className="bg-[#eeee] h-8 w-12 flex items-center justify-center rounded-full">
-          <i className="ri-map-pin-fill"></i>
-        </h2>
-        <h4 className="font-medium">
-          167, Killa Ward Ballarpur-442701, Dist Chandrapur, Near Visapur Road
-        </h4>
-      </div>
+      {locations.map((location, index) => {
+        return (
+          <div
+            key={index}
+            className="gap-3 border-2 border-gray-100 active:border-black rounded-xl flex items-center my-2 p-3 justify-start"
+          >
+            <h2 className="bg-[#eeee] h-8 w-12 flex items-center justify-center rounded-full">
+              <i className="ri-map-pin-fill"></i>
+            </h2>
+            <h4 className="font-medium">{location}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 };
