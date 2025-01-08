11b9eceac17210debb0de4a80516b9de3bd2fb5d
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-08 15:12:45 +0530

    Added GSAP animation to RidePopUp component

--- Frontend/src/components/RidePopUp.jsx
+++ Frontend/src/components/RidePopUp.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const RidePopUp = () => {
+const RidePopUp = (props) => {
   return (
     <div>
-      <h4 className="p-1 text-center w-[93%] absolute top-0">
+      <h4
+        onClick={() => props.setRidePopUP(false)}
+        className="p-1 text-center w-[93%] absolute top-0"
+      >
         <i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
       </h4>
       <h3 className="text-2xl font-semibold mb-6 mt-3">New Ride Available</h3>
--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -2,8 +2,25 @@
 import { Link } from "react-router-dom";
 import CaptainDetails from "../components/CaptainDetails";
 import RidePopUp from "../components/RidePopUp";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef, useState } from "react";
 
 const CaptainHome = () => {
+  const [ridePopUP, setRidePopUP] = useState(true);
+  const ridePopUpRef = useRef(null);
+
+  useGSAP(() => {
+    if (ridePopUP) {
+      gsap.to(ridePopUpRef.current, {
+        transform: "translateY(0)",
+      });
+    } else {
+      gsap.to(ridePopUpRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [ridePopUP]);
   return (
     <div className="h-screen">
       <div className="fixed p-6 flex items-center justify-between w-full">
@@ -27,8 +44,11 @@ const CaptainHome = () => {
       <div className="h-2/5 p-6">
         <CaptainDetails />
       </div>
-      <div className="fixed w-full bg-white  bottom-0 z-10 px-3 py-6">
-        <RidePopUp />
+      <div
+        ref={ridePopUpRef}
+        className="fixed w-full bg-white translate-y-full bottom-0 z-10 px-3 py-6"
+      >
+        <RidePopUp setRidePopUP={setRidePopUP} />
       </div>
     </div>
   );
